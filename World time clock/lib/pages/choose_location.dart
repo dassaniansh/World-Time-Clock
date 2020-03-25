10f79615c6200f249e7b41c3bb5d9c48419bf9e3
dassaniansh <54389313+dassaniansh@users.noreply.github.com>
2020-03-25 19:46:45 +0530

    Added basic screen files

---
+++ World time clock/lib/pages/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChoseLocation extends StatefulWidget {
+  @override
+  _ChoseLocationState createState() => _ChoseLocationState();
+}
+
+class _ChoseLocationState extends State<ChoseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('choose location screen'),
+    );
+  }
+}
